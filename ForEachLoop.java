e6cd0667602be4182defd8c996b7a5f8e2520c68
Ganesh <2023bit501@sggs.ac.in>
2024-01-08 06:13:42 +0530

    For each Loop and Decison Making statements Committed

---
+++ ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String arr[] = {"ganesh","you","me","yourself","mane"};
+        for(String name : arr)
+        {
+            System.out.println(name);
+        }
+    }
+}
