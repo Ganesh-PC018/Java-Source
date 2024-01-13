5bd7b602e2eca816fe7a17d4231db4387a071235
Ganesh <2023bit501@sggs.ac.in>
2024-01-13 06:22:39 +0530

    Committed Task on 13 Jan

---
+++ CanWeExecuteProgramWithoutMainMethod.java
@@ -0,0 +1,10 @@
+public class CanWeExecuteProgramWithoutMainMethod {
+    static
+    {
+        System.out.println("Hello im Here Without Main Method But i'm Unable Run so after 1.7 JDK i can't execute without main");
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ NoMainMethod.java
@@ -0,0 +1,7 @@
+public class NoMainMethod {
+    // This class does not have a main method
+
+    public void someMethod() {
+        System.out.println("This class does not have a main method.");
+    }
+}
